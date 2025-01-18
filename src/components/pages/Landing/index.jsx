ba34f49726a524664c2d5897d0861093a4a4cf14
Jacob Handley <skaarjshark@outlook.com>
2025-01-17 18:13:46 -0600

    added styles file

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,10 +1,12 @@
-// import pieChart from '../../../assets/pie-chart.png';
-// import lineGraph from '../../../assets/line-graph.png';
-// import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+import pieChart from '../../../assets/pie-chart.png';
+import lineGraph from '../../../assets/line-graph.png';
+import barGraph from '../../../assets/bar-graph.png';
+import paperStack from '../../../assets/paper-stack.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
 import {decodeBase64} from '../../../utils/decodeBase64.js';
+
+import './styles.css'
 
 /**
  * TODO: Ticket 1:
@@ -32,8 +34,22 @@ export const LandingPage = () => {
 
   return (
     <div className='flex-c w-[100vw] secondary-c'>
-      Landing Page
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
+      {/* Landing Page
+      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div> */}
+      <div id="title">
+        <h1 id="big-text">Asylum Office Grant Rate Tracker</h1>
+        <h3 id="small-text">The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on Asylum Office decisions</h3>
+      </div>
+      <div id="pictures">
+        <img className="images" src={barGraph} alt="barGraph image"/>
+        <img className="images" src={pieChart} alt="lineGraph image"/>
+        <img className="images" src={lineGraph} alt="pieChart image"/>
+      </div>
+      <div id="pictures-text">
+        <h3>Search Grant Rates By Office</h3>
+        <h3>Search Grant Rates Over Time</h3>
+        <h3>Search Grant Rates By Nationality</h3>
+      </div>
     </div>
   );
 };
