6d02e308d458b75facf1ca5e4bfc06bd379243e2
Jacob Handley <skaarjshark@outlook.com>
2025-01-22 18:53:50 -0600

    Finished the styling (With Tailwind this time)

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -40,47 +40,63 @@ export const LandingPage = () => {
         <h1 className="text-6xl mb-8 text-[#ffffff]">Asylum Office Grant Rate Tracker</h1>
         <h3 className="text-[#ffffff]">The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on Asylum Office decisions</h3>
       </div>
-      <div id="pictures-container">
+      <div className="flex justify-center m-14 gap-20 text-2xl">
         <div>
-          <img className="images" src={barGraph} alt="barGraph image"/>
-          <h3 className="pictures-text">Search Grant Rates By Office</h3>
+          <img className="flex-c gap-3 h-[300px] w-[500px] m-[20px]" src={barGraph} alt="barGraph image"/>
+          <h3>Search Grant Rates By Office</h3>
         </div>
        <div>
-        <img className="images" src={pieChart} alt="pieChart image"/>
-        <h3 className="pictures-text">Search Grant Rates By Nationality</h3>
+        <img className="flex-c gap-3 h-[300px] w-[400px] m-[20px]" src={pieChart} alt="pieChart image"/>
+        <h3>Search Grant Rates By Nationality</h3>
        </div>
        <div>
-        <img className="images" src={lineGraph} alt="lineGraph image"/> 
-        <h3 className="pictures-text">Search Grant Rates Over Time</h3>
+        <img className="flex-c gap-3 h-[300px] w-[500px] m-[20px]" src={lineGraph} alt="lineGraph image"/> 
+        <h3>Search Grant Rates Over Time</h3>
        </div>
       </div>
-      <div id="buttons">
-        <NavLink className="butt" to="/graphs">View the Data</NavLink>
-        <button onClick={downloadCSV} className="butt">Download the Data</button>
+      <div className="flex align-center mx-auto gap-8">
+        <NavLink className="bg-[#aaa] px-[10px] py-[5px] text-white text-md font-semibold" to="/graphs">View the Data</NavLink>
+        <button onClick={downloadCSV} className="bg-[#aaa] px-[10px] py-[5px] text-white text-md font-semibold">Download the Data</button>
       </div>
-      <div id="paper-container">
-        <img className="paper-image" src={paperStack}/>
-        <p className="paper-text">Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May 2021 by the USCIS Asylum Office, which we received through a Freedom of Information Act request. You can search for information on asylum grant rates by year, nationality, and asylum office, visualize the data with charts and heat maps, and download the data set.</p>
-      </div>
-      <h1 id="sdi-big-text">Systemic Disparity Insights</h1>
-      <div id="insights-container">
-        <div>
-          <h2 className="percentage">36%</h2>
-          <h3 className="insight-text">By the end of the Trump administration, the average asylum office grant rate had fallen 36% from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 20202.</h3>
+      <section className="middle-section flex">
+        <div className="flex-1 hrf-img-container content-center p-20">
+          <img className="hrf-img rounded-2xl h-[70%] w-[100%]" src={paperStack}/>
         </div>
-        <div>
-          <h2 className="percentage">5%</h2>
-          <h3 className="insight-text">The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</h3>
+        <div className="middle-section-text-container flex-1 content-center p-20">
+          <p className="text-xl">Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May 2021 by the USCIS Asylum Office, which we received through a Freedom of Information Act request. You can search for information on asylum grant rates by year, nationality, and asylum office, visualize the data with charts and heat maps, and download the data set.</p>
         </div>
-        <div>
-          <h2 className="percentage">6x Lower</h2>
-          <h3 className="insight-text">Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was 6 times lower than the San Francisco asylum office.</h3>
+      </section>
+      <h1 className="text-5xl mb-[50px]">Systemic Disparity Insights</h1>
+      <div className="insights-section-details flex justify-center m-14 gap-20 text-1xl">
+        <div className="flex-c-1 gap-12">
+          <div className="insights-details-header">
+            <h3 className="text-4xl">36%</h3>
+          </div>
+          <div className="insights-details-content">
+            <h3 className="text-lg">By the end of the Trump administration, the average asylum office grant rate had fallen 36% from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 20202.</h3>
+          </div>
+        </div>
+        <div className="flex-c-1 gap-12">
+          <div className="insights-details-header">
+            <h3 className="text-4xl">5%</h3>
+          </div>
+          <div className="insights-details-content">
+            <h3 className="text-lg">The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</h3>
+          </div>
+        </div>
+        <div className="flex-c-1 gap-12">
+          <div className="insights-details-header">
+            <h3 className="text-4xl">6x Lower</h3>
+          </div>
+          <div className="insights-details-content">
+            <h3 className="text-lg">Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was 6 times lower than the San Francisco asylum office.</h3>
+          </div>
         </div>
       </div>
-      <div id="read-more-butt-container">
-        <NavLink className="read-more-butt" to="https://humanrightsfirst.org/">Read More</NavLink>
+      <div className="read-more-butt-container">
+        <NavLink className="primary-c text-white px-4 py-2" to="https://humanrightsfirst.org/">Read More</NavLink>
       </div>
-      <button className="back-to-top-butt" onClick={scrollToTop}>Back To Top ^</button>
+      <button className="p-[64px]" onClick={scrollToTop}>Back To Top ^</button>
     </div>
   );
 };
